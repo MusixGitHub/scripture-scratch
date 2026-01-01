61adcfa888b1626425be5d6f18c260a7a000c9f7
MusixGitHub <71910701+MusixGitHub@users.noreply.github.com>
2026-01-01 18:23:18 +1100

    Update vite.config.ts
    vercel与pages共存

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/scripture-scratch/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/scripture-scratch/' : '/',
   plugins: [react()],
-})
+}))
